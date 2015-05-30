4b1ce7d32f877f2153eb4ff1879c62447c9b0d07
Peter Orum <peter@peterorum.com>
2015-05-30 15:58:25 +1000

    shuffle

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,44 +5,85 @@
 
     var module = angular.module('functal.controllers', []);
 
-    module.controller('ImagesCtrl', ['functalData', '$interval', '$scope', function(functalData, $interval, $scope)
-    {
-        var getImages = function()
+    module.controller('ImagesCtrl', ['functalData', '$interval', '$scope', '$ionicScrollDelegate',
+        function(functalData, $interval, $scope, $ionicScrollDelegate)
         {
-            functalData.getImages().then(function(result)
+            var getImages = function()
             {
-                $scope.images = result.data.images;
-            });
-        };
+                functalData.getImages().then(function(result)
+                {
+                    $scope.images = result.data.images;
+                    sort();
+                });
+            };
 
-        $scope.gotoTop = function()
-        {
-            $window.location.href = '#top';
-        };
+            var sort = function()
+            {
 
-        $scope.showMore = function()
-        {
-            $scope.showCount += 4;
+                $scope.images = R.sort(function(a, b)
+                {
+                    switch ($scope.sorter)
+                    {
+                        case 'asc':
+                            {
+                                return -(b > a ? 1 : b < a ? -1 : 0);
+                            }
 
-            console.log('showmore', $scope.showCount);
+                        case 'desc':
+                            {
+                                return (b > a ? 1 : b < a ? -1 : 0);
+                            }
 
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-        };
+                        case 'shuffle':
+                            {
+                                return Math.random() < 0.5 ? -1 : 1;
+                            }
+                    }
 
-        // reload
+                }, $scope.images);
+            };
 
-        $interval(function()
-        {
+            $scope.gotoTop = function()
+            {
+                $window.location.href = '#top';
+            };
+
+            $scope.showMore = function()
+            {
+                $scope.showCount += 4;
+
+                console.log('showmore', $scope.showCount);
+
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+            };
+
+            $scope.sortBy = function(sorter)
+            {
+                $scope.sorter = sorter;
+
+                sort();
+
+                $scope.showCount = 6;
+
+                $ionicScrollDelegate.scrollTop();
+            };
+
+            // reload
+
+            $interval(function()
+            {
+                getImages();
+            }, 5 * 60000);
+
+            // init
+
+            $scope.cdn = 'https://d1aienjtp63qx3.cloudfront.net/';
+
+            $scope.showCount = 6;
+
+            $scope.sorter = 'desc';
+
             getImages();
-        }, 5 * 60000);
-
-        // init
-
-        $scope.cdn = 'https://d1aienjtp63qx3.cloudfront.net/';
-
-        $scope.showCount = 6;
-
-        getImages();
-
-    }]);
+        }
+    ]);
 })();
