1d6ba783aa980f9311c577214423054b24504bf8
Peter Orum <peter@peterorum.com>
2015-06-01 22:24:24 +1000

    save to camera roll

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,7 +12,12 @@
             {
                 functalData.getImages().then(function(result)
                 {
-                    $scope.images = result.data.images;
+                    $scope.images = R.map(function(i)
+                    {
+                        return {
+                            name: i
+                        };
+                    }, result.data.images);
                     sort();
                 });
             };
@@ -78,12 +83,65 @@
             // init
 
             $scope.cdn = 'https://d1aienjtp63qx3.cloudfront.net/';
+            $scope.s3 = 'https://s3.amazonaws.com/functal-images/';
 
             $scope.showCount = 6;
 
             $scope.sorter = 'shuffle';
 
             getImages();
+
+            // ---------------- save to camera roll
+
+            $scope.save = function(image)
+            {
+                image.status = 'saving to your photos...';
+
+                // todo - keep testing with cdn to see if cors overcome
+                var url = $scope.s3 + image.name;
+
+                convertImgToBase64URL(url, function(base64DataURL)
+                {
+                    CameraRoll.saveToCameraRoll(base64DataURL, function()
+                    {
+                        image.status = 'saved';
+                        $scope.$apply();
+
+                    }, function(err)
+                    {
+                        image.status = 'Error : ' + err;
+                        $scope.$apply();
+
+                    });
+                }, 'image/jpeg');
+            };
+
+            /**
+             * Convert an image
+             * to a base64 url
+             * @param  {String}   url
+             * @param  {Function} callback
+             * @param  {String}   [outputFormat=image/jpeg]
+             * http://stackoverflow.com/questions/6150289/how-to-convert-image-into-base64-string-using-javascript
+             */
+            function convertImgToBase64URL(url, callback, outputFormat)
+            {
+                var img = new Image();
+                img.crossOrigin = 'Anonymous';
+                img.onload = function()
+                {
+                    var canvas = document.createElement('CANVAS'),
+                        ctx = canvas.getContext('2d'),
+                        dataURL;
+                    canvas.height = img.height;
+                    canvas.width = img.width;
+                    ctx.drawImage(img, 0, 0);
+                    dataURL = canvas.toDataURL(outputFormat);
+                    callback(dataURL);
+                    canvas = null;
+                };
+                img.src = url;
+            }
         }
     ]);
 })();
