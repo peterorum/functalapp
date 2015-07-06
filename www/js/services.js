e11a820eb627905506c6bf92509a443f0a033252
Peter Orum <peter@peterorum.com>
2015-07-06 23:16:59 +1000

    add db & votes

--- www/js/services.js
+++ www/js/services.js
@@ -4,16 +4,32 @@
 
     var module = angular.module('functal.services', []);
 
-    module.factory('functalData', ['$http', function($http)
+    module.factory('functalData', ['$http', '$q', function($http, $q)
     {
         var functalData = {};
 
-        functalData.server = 'http://functalserver.codeindeed.com:8083';
-        // functalData.server = 'http://localhost:8083';
+        // functalData.server = 'http://functalserver.codeindeed.com:8083';
+        functalData.server = 'http://localhost:8083';
 
         functalData.getImages = function()
         {
             return $http.jsonp(functalData.server + '/getimages?callback=JSON_CALLBACK');
+        };
+
+        functalData.vote = function(image, like, dislike)
+        {
+            return $http.jsonp(functalData.server + '/vote?callback=JSON_CALLBACK',
+            {
+                params:
+                {
+                    data:
+                    {
+                        name: image.name,
+                        like: like,
+                        dislike: dislike
+                    }
+                }
+            });
         };
 
         return functalData;
